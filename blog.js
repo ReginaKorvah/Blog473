1886571689ed54f8a43728bbc8a7f97f3ec1b8bd
ryan-hartmann <99146284+ryan-hartmann@users.noreply.github.com>
2022-05-01 21:58:21 -0400

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,58 @@
+window.onload = pageLoad;
+
+function pageLoad() {
+	$("dogButton").onclick = dogClick;
+	new Ajax.Request("blog.json",
+	{
+		method:"get",
+		onSuccess: show,
+		onFailue: ajaxFailure,
+		onException: ajaxFailure
+	}
+}
+
+function show(ajax) {
+	var data = JSON.parse(ajax.responseText);
+	for(var i = 0; i < data.Blogs.length; i++) {
+		var h = document.createElement("h1");
+		h.innerHTML = data.Blogs[i].Topic;
+		document.body.appendChild(h);
+		var p = document.createElement("p");
+		h.innerHTML = data.Blogs[i].Post;
+		document.body.appendChild(p);
+		var n = document.createElement("p");
+		h.innerHTML = data.Blogs[i].Name;
+		document.body.appendChild(n);
+	}
+}
+
+function dogClick() {
+	new Ajax.Request("blog.json",
+		{
+			method:"get",
+			onSuccess: add,
+			onFailure: ajaxFailure,
+			onException: ajaxFailure
+		}
+	);
+}
+
+function add(ajax)
+{	
+	var data = JSON.parse(ajax.responseText);
+	for(var i = 0; i < data.Blogs.length; i++)
+	{
+			var p = document.createElement("li");
+			p.innerHTML = data.pets[i].name;
+			document.body.appendChild(p);
+	}
+}
+
+function ajaxFailure(ajax, exception) {
+	alert("Error making Ajax request:" + "\n\nServer status: \n"
+		+ ajax.status + " " + ajax.statusText
+		+ "\n\nServer response text: \n" + ajax.responseText);
+	if (exception) {
+		throw exception;
+	}
+}
