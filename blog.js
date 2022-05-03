f5f069165807c6a0b5c35166cfcdaaa3c8691f2c
Regina Korvah <regina.korvah1@gmail.com>
2022-05-03 19:47:03 -0400

    Javascript Updated for Blog Details

--- blog.js
+++ blog.js
@@ -1,13 +1,13 @@
 window.onload = pageLoad;
 
 function pageLoad(){
-	var startBlog = document.getElementById("new-blog-form");
+	var startBlog = $("new-blog-form");
 	startBlog.style.display = "none";
 
-	var openFormBut = document.getElementById("open-form-button");
+	var openFormBut = $("open-form-button");
 	openFormBut.onclick = openForm;
 
-	var submitBlog = document.getElementById("submit");
+	var submitBlog = $("submit");
 	submitBlog.onclick = submitForm;
 	
 	new Ajax.Request("blog.json",
@@ -16,38 +16,59 @@ function pageLoad(){
 		onSuccess: show,
 		onFailue: ajaxFailure,
 		onException: ajaxFailure
-	}
+	});
 }
 
 function show(ajax) {
 	var data = JSON.parse(ajax.responseText);
-	for(var i = 0; i < data.Blogs.length; i++) {
-		var h = document.createElement("h1");
-		h.innerHTML = data.Blogs[i].Topic;
-		document.body.appendChild(h);
-		var p = document.createElement("p");
-		h.innerHTML = data.Blogs[i].Post;
-		document.body.appendChild(p);
-		var n = document.createElement("p");
-		h.innerHTML = data.Blogs[i].Name;
-		document.body.appendChild(n);
+	var blog = $("blog");
+
+	for(var i = 0; i < data.Blogs.length; i++){
+		var blogPost = document.createElement("div");
+		blogPost.className = "blog-post";
+		blog.appendChild(blogPost);
+
+		var titleLink = document.createElement("a");
+		titleLink.href = "javascript:;";
+		titleLink.onclick = "showPost();";
+		titleLink.innerHTML = "Blog: " + data.Blogs[i].Topic;
+
+		var title = document.createElement("h2");
+		title.id = "blog-title";
+		title.className = "blog-title"
+		blogPost.appendChild(title);
+		title.appendChild(titleLink);
+
+		var blogContent = document.createElement("div");
+		blogContent.className = "blog-content";
+		blogPost.appendChild(blogContent);
+
+		var blogHead = document.createElement("div");
+		blogHead.className = "blog-head";
+		blogContent.appendChild(blogHead);
+
+		var image = document.createElement("img");
+		image.className = "user-icon";
+		image.src = "userimage.png";
+		blogHead.appendChild(image);
+
+		var name = document.createElement("h4");
+		name.className = "user-name";
+		name.innerHTML = data.Blogs[i].Name;
+		blogHead.appendChild(name);
+
+		var blogText = document.createElement("p");
+		blogText.className = 'blog-text';
+		blogText.innerHTML = data.Blogs[i].Post;
+		blogContent.appendChild(blogText);
 	}
 }
 
-function dogClick() {
-	new Ajax.Request("blog.json",
-		{
-			method:"get",
-			onSuccess: add,
-			onFailure: ajaxFailure,
-			onException: ajaxFailure
-		}
-	);
-}
 
-function add(ajax){	
+function add(ajax){
+	var today = new Date();	
 	var data = JSON.parse(ajax.responseText);
-	data.stringify({Name: $("name").value, Topic: $("Topic").value, Post: $("Post").value});
+	data.stringify({Name: $("name").value, Topic: $("topic").value, Post: $("post").value, Likes: 0});
 }
 
 function ajaxFailure(ajax, exception) {
@@ -62,7 +83,7 @@ function ajaxFailure(ajax, exception) {
 function openForm(){
 	this.style.display = "none";
 
-	var form = document.getElementById('new-blog-form');
+	var form = $('new-blog-form');
 	form.style.display = "";
 }
 
@@ -70,15 +91,19 @@ function submitForm(){
 	var filled = checkForm();
 	
 	if(filled){
-		
+		new Ajax.Request("blog.json",
+		{
+			method:"get",
+			onSuccess: add,
+			onFailure: ajaxFailure,
+			onException: ajaxFailure
+		}
+	);
 
-
-
-
-		let button = document.getElementById("open-form-button");
+		let button = $("open-form-button");
 		button.style.display = "";
 
-		let form = document.getElementById("new-blog-form");
+		let form = $("new-blog-form");
 		form.style.display = "none";
 	}
 	
